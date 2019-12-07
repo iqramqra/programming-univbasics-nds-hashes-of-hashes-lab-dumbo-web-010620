3d1215da826b6bab9ff53f7bce7e85d1b6f536f9
Iqra Masroor <masrooriqra@gmail.com>
2019-12-07 01:52:00 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -40,6 +40,19 @@ def naming_system
      }
    }
  }
+  PORTION_2 = {
+    label: "Order"
+  }
+  PORTION_3 = {
+    label: "Family",
+      sub_category: {
+       label: "Genus",
+   }
+  }
+  PORTION_4 = {
+    label: "Species",
+    sub_category: nil
+  }
   # Remember:
   #  Kingdom
   #  Phylum
